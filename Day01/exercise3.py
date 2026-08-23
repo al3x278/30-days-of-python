8607a14feb5cbfccb17f1a48d04cc8d941591d10
Alex Lee <249377561+al3x278@users.noreply.github.com>
2026-08-23 16:14:53 -0700

    Day 1: Completed exercises

--- Day01/exercise3.py
+++ Day01/exercise3.py
@@ -1,4 +1,23 @@
 # Exercise 3
 
 # Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+
+import math
+
+print("Integer: 5")
+print("Float: 5.6")
+print("Complex: 5 + 1j")
+print("String: 'Coding is fun'")
+print("Boolean: True")
+print("List: [2,4,6,8]")
+print("Tuple: (2,4,6,8)")
+print("Set: {2,4,6,10}")
+print("Dictionary: {'name': 'Alex', 'age': 25, 'country': 'United States'}")
+
 # Find an Euclidean distance between (2, 3) and (10, 8)
+
+point1 = (2, 3)
+point2 = (10, 8)
+dist = (point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2
+distance = math.sqrt(dist)
+print("Euclidean distance:", distance)
